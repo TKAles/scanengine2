33ea434098decb24ef7d1489e0dc77b2b9fd6d7f
Thomas Ales <tkales@iastate.edu>
2020-03-24 05:35:57 -0500

    synch

--- ScanengineViewModel.cs
+++ ScanengineViewModel.cs
@@ -7,27 +7,57 @@ using System.ComponentModel;
 
 namespace wpfscanengine
 {
-    public partial class ScanengineViewModel : INotifyPropertyChanged
+    public partial class ScanengineViewModel 
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public string CurrentMLSSerial
         {
             get { return this.MLSStage.SerialNumber; }
             set
-            {
-                this.MLSStage.SerialNumber = value;
-                Console.WriteLine("Property SerialNumber changed to:" + this.MLSStage.SerialNumber);
-            }
+            { this.MLSStage.SerialNumber = value; }
         }
-
         MLS203Stage MLSStage;
 
-        public ScanengineViewModel(string _ser = "73000001")
+        private string _msoaddr;
+        public string CurrentMSOAddress
+        {
+            get { return this._msoaddr; }
+            set { this._msoaddr = value; }
+        }
+
+        private double _xo = 0.000f;
+        private double _yo = 0.000f;
+        private double _xd = 0.000f;
+        private double _yd = 0.000f;
+
+        public double XOrigin
+        {
+            get { return _xo; }
+            set { _xo = value; }
+        }
+        public double YOrigin
+        {
+            get { return _yo; }
+            set { _yo = value; }
+
+        }
+        public double XDelta
+        {
+            get { return _xd; }
+            set { _xd = value; }
+        }
+        public double YDelta
+        {
+            get { return _yd; }
+            set { _yd = value; }
+        }
+
+        public ScanengineViewModel(string _ser = "73000001", string _mso = "not set!")
         {
             this.MLSStage = new MLS203Stage();
             this.CurrentMLSSerial = _ser;
+            this.CurrentMSOAddress = _mso;
 
         }
+
     }
 }
